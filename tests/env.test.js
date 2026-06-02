5eeb0676fb43dd970a3f1c278f8b7b31886b1195
Bhargavi N <bhargavi2048@gmail.com>
2026-06-02 12:06:25 +0530

    Delete tests directory

--- tests/env.test.js
+++
@@ -1,9 +0,0 @@
-const { validateEnv, schema } = require("../src/config/env");
-
-describe("env validation", () => {
-  test("applies defaults", () => {
-    delete process.env.RATE_LIMIT_MAX;
-    validateEnv();
-    expect(process.env.RATE_LIMIT_MAX).toBe(schema.defaults.RATE_LIMIT_MAX);
-  });
-});
